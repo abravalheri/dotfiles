2d666f7c65a3076beb1a7d5ffaf379da0cb67187
Anderson Bravalheri <andersonbravalheri@gmail.com>
2015-03-19 21:07:56 -0300

    Add plugins, specially for rails development

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,4 +3,4 @@
 # # Example format: plugins=(rails git textmate ruby lighthouse)
 # # Add wisely, as too many plugins slow down shell startup.
 #
-plugins=(git rbenv ruby rails python pip)
+plugins=(gitfast git-flow git-extras rbenv ruby rails rake-fast python pip node)
