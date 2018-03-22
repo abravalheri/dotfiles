14ffa5d72fb72b7cd216d2df28316ec10048daee
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-03-22 18:41:03 +0000

    Fix xinit issue with distributed conf
    Since the other the scripts are executed depends on the name,
    it is specially important to exec bspwm as the last script.

--- bspwm/.config/X11/xinit/xinitrc.d/+bspwm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# The name of this file starting with + is important to make sure
-# it runs after the other ones
-
-start-pulseaudio-x11  # used for polybar and volume hotkeys
-
-exec bspwm
