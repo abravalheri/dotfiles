573aa0e60cff8264f777227dc5c8c2b40f0fa0e9
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-03-23 09:36:41 +0000

    Avoid calling programs that might be not installed

--- keyboard/.config/X11/xinit/xinitrc.d/keyboard.sh
+++ keyboard/.config/X11/xinit/xinitrc.d/keyboard.sh
@@ -3,16 +3,21 @@
 usermodmap=$HOME/.config/X11/xinit/.Xmodmap
 sysmodmap=/etc/X11/xinit/.Xmodmap
 
-if [ -f $sysmodmap ]; then
-  xmodmap $sysmodmap
+# Load modified key maps
+if command -v xmodmap >/dev/null; then
+  if [ -f $sysmodmap ]; then
+    xmodmap $sysmodmap
+  fi
+
+  if [ -f "$usermodmap" ]; then
+    xmodmap "$usermodmap"
+  fi
 fi
 
-if [ -f "$usermodmap" ]; then
-  xmodmap "$usermodmap"
+# Share modifiers with standalone keys using xcape
+if command -v xcape >/dev/null; then
+  xcape -e 'Control_L=Escape'
+  xcape -e 'Super_L=Tab'
+  xcape -e 'Hyper_L=Escape'
+  xcape -e 'Meta_R=Multi_key'
 fi
-
-# share modifiers with standalone keys using xcape
-xcape -e 'Control_L=Escape'
-xcape -e 'Super_L=Tab'
-xcape -e 'Hyper_L=Escape'
-xcape -e 'Meta_R=Multi_key'
