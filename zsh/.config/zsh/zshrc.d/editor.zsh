ef9f609183b0278bf8aa94bf99fe6d26f91b11e3
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-06-25 21:57:45 +0000

    Avoid nvr It is very hard to manage in multiple screens...

--- zsh/.config/zsh/zshrc.d/editor.zsh
+++ zsh/.config/zsh/zshrc.d/editor.zsh
@@ -1,9 +1,7 @@
 #!/usr/bin/env zsh
 # vim: set foldmethod=marker :
 
-if command-exists nvr; then
-  export EDITOR='nvr -s'
-elif command-exists nvim; then
+if command-exists nvim; then
   export EDITOR='nvim'
 elif command-exists vim; then
   export EDITOR='vim'
