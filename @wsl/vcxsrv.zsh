8aee0fd4c8ca777e047d2f7dde004331af5301cf
Anderson Bravalheri <andersonbravalheri@gmail.com>
2019-07-06 11:05:35 +0100

    Improve vcxsrv lauching
    - Add script to configure keyborad properly using xmodmap
    - Use rofi to sudo start dbus

--- @wsl/vcxsrv.zsh
+++ @wsl/vcxsrv.zsh
@@ -1,21 +1,74 @@
 #!/usr/bin/env zsh
-process-running() {
+linux-process-running() {
   local PID=$1
   kill -s 0 "$PID" &>/dev/null
+}
+
+windows-process-running() {
+  tasklist.exe | grep -q "$1"
+}
+
+load-keyboard() {
+  local DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  local -U xmodmap_candidates=(
+    "$DIR/.xmodmap-us-102-tal-enter"
+    "$DOTFILES/@wsl/.xmodmap-us-102-tal-enter"
+    "$HOME/.dotfiles/@wsl/.xmodmap-us-102-tal-enter"
+  )
+  for fp in $xmodmap_candidates; do
+    if [[ -f $fp ]]; then
+      DISPLAY=:0 xmodmap "$fp"
+      echo "$fp loaded" >>/dev/stderr
+      break
+    fi
+  done
+}
+
+start-dbus() {
+  if [ -f ~/.local/bin/rofi-askpass ]; then
+    DISPLAY=:0 SUDO_ASKPASS=~/.local/bin/rofi-askpass sudo -A service dbus start
+  else
+    echo "Sudo required to start dbus" >>/dev/stderr
+  fi
+}
+
+vcxsrv-is-running() {
+  if [[ ! -f "$VCXSRV_PID" ]]; then
+    echo "$VCXSRV_PID not found." >>/dev/stderr
+    return 1
+  fi
+
+  if ! windows-process-running "vcxsrv.exe"; then
+    echo "$VCXSRV_PID found, but vcxsrc.exe not running." >>/dev/stderr
+    return 1
+  fi
+
+  return 0
 }
 
 vcxsrv-init() {
   # Initilize VcXsrv unless it is already running
   local VCXSRV_ROOT=$1
   local RUNTIME_DIR="${XDG_RUNTIME_DIR:-/tmp/run/$(id -u)}"
-  mkdir -p "$RUNTIME_DIR"
   local VCXSRV_PID="$RUNTIME_DIR/vcxsrv.pid"
+  mkdir -p "$RUNTIME_DIR"
 
-  if [[ ! -f "$VCXSRV_PID" ]] || ! process-running "$(< "${VCXSRV_PID}")"; then
-    "${VCXSRV_ROOT}/vcxsrv.exe" :0 -dpi auto -multiwindow -clipboard -wgl -silent-dup-error -xkblayout us -xkbvariant altgr-intl -xkbvariant nodeadkeys  &>/dev/null &
-    # sudo /etc/init.d/dbus start
+  if ! vcxsrv-is-running; then
+    "${VCXSRV_ROOT}/vcxsrv.exe" :0 \
+       -dpi auto \
+       -multiwindow \
+       -clipboard \
+       -wgl \
+       -silent-dup-error \
+       -xkblayout us \
+       -xkbvariant altgr-intl \
+       -xkbvariant nodeadkeys \
+       -xkboptions compose:rctrl &>/dev/null &
     PID=$!
     echo "$PID" > "$VCXSRV_PID"
+    sleep 0.3  # wait for X to boot-up before trying to load the keyboard
+    load-keyboard
+    start-dbus
   fi
 
   export DISPLAY=:0
@@ -25,6 +78,7 @@ vcxsrv-init() {
 
 function() {
   local VCXSRV_ROOT=/mnt/c/Local/Apps/VcXsrv
+
   if [[ -d "$VCXSRV_ROOT" ]]; then
     vcxsrv-init "$VCXSRV_ROOT"
   fi
